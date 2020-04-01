16f9c9ae7a1fb0d15d17f3b1da0a271400ae6551
Caislino2@gmail.com <Caislino2@gmail.com>
2020-04-01 14:46:11 -0700

    wednesday

---
+++ monday.js
@@ -0,0 +1,60 @@
+({
+    babel: true
+})
+
+var abc = 123;
+
+abc
+    
+    
+    var name = 'ramon'
+   var car = 'volkswagen'
+   var age = '40'
+   var address = '123 Main Street'
+
+   var sentence = 'hello my name is' + name + 'and i drive a volkswagen' + 'i am' + age + 'years of age and i live on' + address ;
+
+   console.log(sentence) 
+
+   
+
+   var total = 10;
+   var paid = 3;
+   var balance = total - paid
+
+
+   var item = 'i purchased something for ' + total + '. I paid ' + paid + '. I own ' + balance
+
+   console.log(item)
+
+
+  console.log(item.length)
+
+  console.log(item.substring)
+
+  var index = item.indexOf('n')
+  console.log(index)
+  var itemSubstring = item.substring(5, index)
+  console.log(itemSubstring)
+
+
+  var item2 = item.slice(0, index)
+  console.log(item2);
+
+  var arr2 = ["bob", "nancy", "jim", "scott"];
+  
+  var name = 'johnson'
+
+  arr2.unshift(name2)
+
+  arr2
+
+
+  var num = 2;
+  var pre = arr2.slice(0, num)
+  var post
+  var post = arr2.slice(num)
+  console.log(pre)
+
+  console.log(post)
+  
