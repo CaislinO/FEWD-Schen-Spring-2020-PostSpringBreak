0d5aa63e49df96d9bcff89a039c1935af4586c5f
Caislino2@gmail.com <Caislino2@gmail.com>
2020-04-06 18:30:51 -0700

    new pull

--- monday.js
+++ monday.js
@@ -1,12 +1,3 @@
-({
-    babel: true
-})
-
-var abc = 123;
-
-abc
-    
-    
     var name = 'ramon'
    var car = 'volkswagen'
    var age = '40'
@@ -43,7 +34,7 @@ abc
 
   var arr2 = ["bob", "nancy", "jim", "scott"];
   
-  var name = 'johnson'
+  var name2 = 'johnson'
 
   arr2.unshift(name2)
 
@@ -52,12 +43,136 @@ abc
 
   var num = 2;
   var pre = arr2.slice(0, num)
-  var post
   var post = arr2.slice(num)
   console.log(pre)
 
   console.log(post)
 
 
+
   
+
+///////////////////////////////////////////////
+
+////// Equality Operations and Booleans ///////
+
+///////////////////////////////////////////////
+
+
+// Create a function for each of the following statements
+
+//  for equality
+
+// const xIsLessThanY = function (x, y) {
+//     if (x > y) { // if this is a true condition
+    
+//         return x; 
+//     } else {
+
+//         return "x is not greater than y";
+//     }
+// } 
+
+
+
+
+
+/**
+ * x is greater than y
+
+ * 
+
+ */
+
+//     ie: 
+
+//     const xGreaterThanY = function (x, y) {
+
+//       if (x > y) {  // if this is a true condition
+
+//         return x;   // return x
+
+//       } else {
+
+//         return "x is not greater than y";
+
+//       }
+
+//     };
+
+// };
+
+
+// const ​xDividedByY = function(x, y) {
+//     if (x > y) { // if this is a true condition
+    
+//         return x; 
+//     } else {
+
+//         return "x is not greater than y";
+//     }
+// } 
+
+/**
+ * x divided by y is less than or equal to y divided by x
+ * @returns {Boolean}
+ */
+
+/**
+ * not zero is not equal to zero divided by zero
+ * @returns {Boolean}
+ */
+
+/**
+
+ * Create a variable named fullName.
+
+ * Set the value of fullName to your full name.
+
+ * Check if x minus y is equal to the length of "length of fullName"
+
+ * @returns {Boolean}
+ */
+/**
+
+ * Create a variable named fullName.
+
+ * Set the value of fullName to your full name.
+
+ * chceck if x is less than or equal than fullName.length
+
+ * @returns {Boolean}
+
+ */
+
+/**
+
+ * thirteen plus six is not equal to twentythree minus 5
+
+ * @returns {Boolean}
+
+ */
+
+/**
+
+ * seventyfive or seventyfive and twentyfour times three
+
+ * @returns {Number}
+
+ */
+
+/**
+ * "my first name" is not equal to "My first name"
+ * @returns {Boolean}
+ */
+
+ /**
+ * four plus three and six times eight
+ * @returns {Number}
+ */
+
+/**
+ * "four" is equal to 4
+ * @returns {Boolean}
+ */
   
